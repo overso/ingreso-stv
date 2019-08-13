f967edf8b94869556a4e785325a171be931797ac
Oscar Jiza <oscarjiza@iMac-de-Moises.local>
2019-08-13 18:38:17 -0500

    Se crean las dos vistas principales.

---
+++ src/MovieDetail.js
@@ -0,0 +1,49 @@
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+import { detailMovie } from './actions';
+import Genres from "./components/Genres";
+import Roles from "./components/Roles";
+import TitleDetail from "./components/TitleDetail";
+import { Link } from "react-router-dom";
+
+class MovieDetail extends PureComponent {
+  constructor(props) {
+    super(props);
+    const { movieId } = this.props.match.params
+    this.props.detailMovie(movieId);
+  }
+
+
+  render() {
+    if (!this.props.movies.selectedMovie) {
+        return <div><h1>Cargando ...</h1></div>;
+    }
+    const { selectedMovie } = this.props.movies;
+    console.log(selectedMovie);
+    return (
+        <div className="wrapper">
+            <div className="box header">
+                <Link to={`/movies/`}><h2>regresar</h2></Link>
+                <h2>{selectedMovie.title}</h2>
+            </div>
+            <div className="box sidebar"><img src={selectedMovie.image_medium} alt={selectedMovie.image_small_alt} /></div>
+            <div className="box content">
+                <TitleDetail 
+                    title={selectedMovie.title}
+                    publishyear={selectedMovie.extendedcommon.media.publishyear} 
+                    ratingCode={selectedMovie.extendedcommon.media.rating.code}
+                    duration={selectedMovie.extendedcommon.media.duration}
+                />
+                <Genres genres={selectedMovie.extendedcommon.genres.genre}/>
+                <Roles roles={selectedMovie.extendedcommon.roles.role} />
+                Título Original: {selectedMovie.extendedcommon.media.originaltitle}
+            </div>
+        </div>
+    );
+  }
+}
+
+const mapStateToProps = ({ movies }) => ({ movies });
+const mapDispatchToPropsParam = { detailMovie };
+
+export default connect(mapStateToProps, mapDispatchToPropsParam)(MovieDetail);
---
+++ src/MovieList.js
@@ -0,0 +1,38 @@
+import React, { PureComponent } from 'react';
+import MovieResults from "./components/MovieResults";
+import SearchInput from "./components/SearchInput";
+import Logo from "./components/Logo";
+import { connect } from 'react-redux';
+import { fetchMovies, filterMovie } from './actions';
+import './App.css';
+
+class MovieList extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.props.fetchMovies();
+  }
+
+  handleSearchChange = event => {
+    const data = this.props.movies ? this.props.movies.data : [];
+    this.props.filterMovie(data, event.target.value, 40);
+  };
+
+  render() {
+    console.log(this.props.movies);
+    return (
+      <div>
+        <Logo 
+          src="https://www.clarovideo.com/webclient/sk_core/images/clarovideo-logo-sitio.svg"
+          alt="ClaroVideo"
+        />
+        <SearchInput textChange={this.handleSearchChange} />
+        <MovieResults movies={this.props.movies ? this.props.movies.filteredMovies : []}/>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = ({ movies }) => ({ movies });
+const mapDispatchToPropsParam = { fetchMovies, filterMovie };
+
+export default connect(mapStateToProps, mapDispatchToPropsParam)(MovieList);
