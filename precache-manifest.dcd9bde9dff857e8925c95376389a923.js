92d6f9a0461b1ce8b5c2155be4873eeef58b2db2
Oscar Jiza <oscarjiza@iMac-de-Moises.local>
2019-08-13 19:37:19 -0500

    Updates

---
+++ precache-manifest.dcd9bde9dff857e8925c95376389a923.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da331fcc40875eb05c1a1c9794a7de83",
+    "url": "/ingreso-stv/index.html"
+  },
+  {
+    "revision": "cc461529b16e358e84fa",
+    "url": "/ingreso-stv/static/css/main.9296b0f6.chunk.css"
+  },
+  {
+    "revision": "578ea76c852b54f3011a",
+    "url": "/ingreso-stv/static/js/2.b472fd25.chunk.js"
+  },
+  {
+    "revision": "cc461529b16e358e84fa",
+    "url": "/ingreso-stv/static/js/main.1bdb53ab.chunk.js"
+  },
+  {
+    "revision": "582bef2702066a251b1d",
+    "url": "/ingreso-stv/static/js/runtime~main.67640124.js"
+  }
+]);
